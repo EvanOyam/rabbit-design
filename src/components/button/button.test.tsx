70c39a9b657222ed490d86a634b4ca4fb2cf74e4
欧阳智聪 <ouyangzhicong@bytedance.com>
2021-06-05 14:20:32 +0800

    test(button): update button test fn

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,54 +1,62 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Button, { ButtonProps } from './button'
-const defaultProps = {
-  onClick: jest.fn()
-}
 
-const testProps: ButtonProps = {
-  btnType: 'primary',
-  size: 'lg',
-  className: 'klass'
-}
-
-const disabledProps: ButtonProps = {
-  disabled: true,
-  onClick: jest.fn()
-}
-describe('test Button component', () => {
-  it('should render the correct default button', () => {
-    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
-    const element = wrapper.getByText('Nice') as HTMLButtonElement
+describe('Test button component', () => {
+  it('should render a default button', () => {
+    const wrapper = render(<Button>Default</Button>)
+    const element = wrapper.getByText('Default') as HTMLButtonElement
     expect(element).toBeInTheDocument()
-    expect(element.tagName).toEqual('BUTTON')
+    expect(element.tagName).toBe('BUTTON')
     expect(element).toHaveClass('btn btn-default')
-    expect(element.disabled).toBeFalsy()
-    fireEvent.click(element)
-    expect(defaultProps.onClick).toHaveBeenCalled()
+    expect(element).toHaveTextContent(/^Default$/)
   })
-  it('should render the correct component based on different props', () => {
-    const wrapper = render(<Button {...testProps}>Nice</Button>)
-    const element = wrapper.getByText('Nice')
+
+  it('should render a primary-lg button with props', () => {
+    const props: ButtonProps = {
+      className: 'custom-class',
+      btnType: 'primary',
+      size: 'lg'
+    }
+    const wrapper = render(<Button {...props}>Primary</Button>)
+    const element = wrapper.getByText('Primary') as HTMLButtonElement
     expect(element).toBeInTheDocument()
-    expect(element).toHaveClass('btn-primary btn-lg klass')
+    expect(element.tagName).toBe('BUTTON')
+    expect(element).toHaveClass('btn btn-primary custom-class btn-lg')
+    expect(element).toHaveTextContent(/^Primary$/)
   })
-  it('should render a link when btnType equals link and href is provided', () => {
-    const wrapper = render(
-      <Button btnType='link' href='http://dummyurl'>
-        Link
-      </Button>
-    )
-    const element = wrapper.getByText('Link')
+
+  it('should render a link button', () => {
+    const props: ButtonProps = {
+      btnType: 'link',
+      href: 'mock'
+    }
+    const wrapper = render(<Button {...props}>Link</Button>)
+    const element = wrapper.getByText('Link') as HTMLAnchorElement
     expect(element).toBeInTheDocument()
-    expect(element.tagName).toEqual('A')
+    expect(element.tagName).toBe('A')
     expect(element).toHaveClass('btn btn-link')
+    expect(element).toHaveTextContent(/^Link$/)
   })
-  it('should render disabled button when disabled set to true', () => {
-    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
-    const element = wrapper.getByText('Nice') as HTMLButtonElement
-    expect(element).toBeInTheDocument()
-    expect(element.disabled).toBeTruthy()
+
+  it('has been clicked', () => {
+    const props: ButtonProps = {
+      onClick: jest.fn()
+    }
+    const wrapper = render(<Button {...props}>Click</Button>)
+    const element = wrapper.getByText('Click') as HTMLButtonElement
     fireEvent.click(element)
-    expect(disabledProps.onClick).not.toHaveBeenCalled()
+    expect(props.onClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('cannot be clicked when disabled set to true', () => {
+    const props: ButtonProps = {
+      disabled: true,
+      onClick: jest.fn()
+    }
+    const wrapper = render(<Button {...props}>Disabled</Button>)
+    const element = wrapper.getByText('Disabled') as HTMLButtonElement
+    fireEvent.click(element)
+    expect(props.onClick).not.toHaveBeenCalledTimes(1)
   })
 })
