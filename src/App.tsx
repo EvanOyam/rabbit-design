390b93b8320d115b47754ada4bdf95b633e329fa
EvanOyam <953328679@qq.com>
2021-06-05 12:35:56 +0800

    feat(button): button test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Button from "./components/button/button";
+import React from 'react'
+import Button from './components/button/button'
 
 function App() {
   return (
-    <div className="App">
-      <Button btnType="primary">Hello</Button>
+    <div className='App'>
+      <Button btnType='primary'>Hello</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
